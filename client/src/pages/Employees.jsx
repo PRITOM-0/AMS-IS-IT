ef4f412f0b16e7fa51fa7bbdb89ff611eda4b5cf
Pritom Saha <pritomsahawww@gmail.com>
2026-08-04 17:29:33 +0600

    feat: Update asset status and assignment details; enhance employee list layout

--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -83,7 +83,7 @@ function Employees() {
       </div>
 
       {/* List */}
-      <div className="grid gap-4">
+      <div className="grid grid-cols-2 gap-4">
         {filteredEmployees.map((emp) => (
           <EmployeeCard key={emp.id} employee={emp} />
         ))}
