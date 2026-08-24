6566cd24a9292cbd39383c758552191f43a68426
Pritom Saha <pritomsahawww@gmail.com>
2026-08-24 14:34:46 +0600

    feat(AssetDetails): update user display format and enhance styling for user list

--- client/src/pages/AssetDetails.jsx
+++ client/src/pages/AssetDetails.jsx
@@ -769,9 +769,9 @@ const Info = ({
           .map((user, index) => (
             <span
               key={index}
-              className="inline-flex items-center px-2.5 py-1 rounded-md text-xs font-medium bg-slate-100 text-slate-700 border border-slate-200 w-fit"
+              className="inline-flex items-center px-2.5 py-1 rounded-md text-xs font-medium bg-slate-100 text-slate-700 border border-blue-500 w-fit"
             >
-              {user}
+              {index+1}. {user}
             </span>
           ))}
       </div>
