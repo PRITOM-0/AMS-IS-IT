0a93172efa3c0d9c079994b4e8b64fdd83602954
Pritom Saha <pritomsahawww@gmail.com>
2026-09-06 17:11:44 +0600

    feat: Add new user and update header styling for improved UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -41,7 +41,7 @@ function Header({ onLogout, setIsLoggedIn }) {
   }
 };
   return (
-    <header className="fixed top-0 left-0 w-full h-16 bg-white shadow-md flex items-center justify-between pl-2 pr-6 z-50">
+    <header className="fixed top-0 left-0 w-full h-16 bg-white shadow-md flex items-center justify-between pl-2 pr-6 z-50 border-b border-indigo-500">
       
       {/* Left: Logo + Name */}
       <div
